7e1157bca5a1de6732b7fdfed37822f42cb8ea57
khal <khanifkhalid@gmail.com>
2022-12-07 13:58:28 +0300

    ui lesson

---
+++ UILesson1/app/src/main/java/com/example/uilesson1/CollapsingToolBar.java
@@ -0,0 +1,14 @@
+package com.example.uilesson1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CollapsingToolBar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colapsing_tool_bar);
+    }
+}
---
+++ UILesson1/app/src/main/java/com/example/uilesson1/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.uilesson1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setOnClickListener(v -> {
+            toastCall();
+        });
+
+
+        TextView textView2 = findViewById(R.id.textView2);
+        textView2.setOnClickListener(v -> {
+            snackBarCall(v);
+        });
+
+        TextView textView3 = findViewById(R.id.textView3);
+        textView3.setOnClickListener(v->{
+            Intent intent = new Intent(this,CollapsingToolBar.class);
+            startActivity(intent);
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = new MenuInflater(this);
+        inflater.inflate(R.menu.toolbar_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void snackBarCall(View v){
+        Snackbar snackbar = Snackbar.make(v,"Our SnackBar",Snackbar.LENGTH_INDEFINITE);
+        snackbar.setActionTextColor(getResources().getColor(R.color.lime_green));
+        View snackBarView = snackbar.getView();
+        TextView snBrText = snackBarView.findViewById(com.google.android.material.R.id.snackbar_text);
+        snBrText.setTextColor(getResources().getColor(R.color.lime_green));
+
+        View.OnClickListener listener  = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                snackbar.dismiss();
+            }
+        };
+
+        snackbar.setAction("Hide",listener);
+        snackbar.show();
+    }
+
+    private void toastCall(){
+        Toast toast = Toast.makeText(this,"Custom toast",Toast.LENGTH_LONG);
+
+        View customToast = toast.getView();
+
+        TextView textCT = customToast.findViewById(android.R.id.message);
+        textCT.setTextSize(16);
+        textCT.setTextColor(getResources().getColor(R.color.lime_green));
+        textCT.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_launcher_foreground,0,0,0);
+        toast.setGravity(Gravity.CENTER|Gravity.BOTTOM,0,0);
+        customToast.getBackground().setTint(getResources().getColor(R.color.gray));
+        toast.show();
+    }
+}
